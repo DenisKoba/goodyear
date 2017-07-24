2a074e93ad9725984b53d46963ca86e0b601c449
griffin <griffin@zeoalliance.com>
2017-07-24 10:55:49 +0300

    last commit

--- goodyear.js
+++ goodyear.js
@@ -1,3 +1,13 @@
-/**
- * Created by griffin on 7/15/17.
- */
+$("#arrow").on("click", function() {
+    $("html, body").animate({
+        scrollTop: $("#second-block").offset().top
+    }, 500);
+})
+
+$(window).scroll( function(){
+    var top_of_object = $('.js-trigger').offset().top;
+    var top_of_window = $(window).scrollTop();
+    if( top_of_window > top_of_object){
+        $('.good-fourth-section__wrapper_idea-heading').css({"margin": "30px 0 20px 80px"});
+    }
+});
